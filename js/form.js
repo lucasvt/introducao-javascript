4c36e29648cb9b86c040d809112589e8be8483a8
lucas <lucasvt2@gmail.com>
2017-08-26 00:17:06 -0300

    Finalizando a 6 aula

--- js/form.js
+++ js/form.js
@@ -22,10 +22,13 @@ botaoAdicionar.addEventListener("click", function (event) {
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
 
 function exibeMensagensDeErro(erros) {
     var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
     erros.forEach(function (erro) {
         var li = document.createElement("li");
         li.textContent = erro;
@@ -69,12 +72,28 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
     if(!validaPeso(paciente.peso)){
        erros.push("Peso é inválido!");
     }
 
     if(!validaAltura(paciente.altura)){
         erros.push("Altura é inválido!");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("A peso não pode ser em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura não pode ser em branco");
     }
     return erros;
 }
